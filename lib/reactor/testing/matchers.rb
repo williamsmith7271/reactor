eb1a0c1461956fd3057e87db5f4cc60c535a9133
williamsmith7271 <williamsmith7271@gmail.com>
2013-07-08 11:39:36 -0700

    add publish_events rspec matcher and simplify publish_event
    TODO: consolidate this logic?

--- lib/reactor/testing/matchers.rb
+++ lib/reactor/testing/matchers.rb
@@ -1,12 +1,24 @@
 RSpec::Matchers.define :publish_event do |name, data = {}|
 
   match do |block|
-    if data.empty?
-      Reactor::Event.should_receive(:publish).with do |*args|
-        args.first.should == name
+    defaults = {:actor => anything, :target => anything}
+    Reactor::Event.should_receive(:publish).with(name, defaults.merge(data))
+    block.call
+  end
+end
+
+RSpec::Matchers.define :publish_events do |*args|
+
+  match do |block|
+    defaults = {:actor => anything, :target => anything}
+
+    args.each do |event|
+      case event
+        when Symbol
+          Reactor::Event.should_receive(:publish).with(event, anything)
+        when Hash
+          Reactor::Event.should_receive(:publish).with(event.keys.first, defaults.merge(event.values.first))
       end
-    else
-      Reactor::Event.should_receive(:publish).with(name, data)
     end
     block.call
   end
