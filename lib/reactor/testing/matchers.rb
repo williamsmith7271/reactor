2b8cde2028611ba313307cac28a88e8d0ae99cc7
williamsmith7271 <williamsmith7271@gmail.com>
2013-07-08 13:03:15 -0700

    matcher only defaults actor to anything

--- lib/reactor/testing/matchers.rb
+++ lib/reactor/testing/matchers.rb
@@ -1,8 +1,8 @@
 RSpec::Matchers.define :publish_event do |name, data = {}|
 
   match do |block|
-    defaults = {:actor => anything, :target => anything, at: nil}
-    Reactor::Event.should_receive(:publish).with(name, defaults.merge(data))
+    defaults = {:actor => anything}
+    Reactor::Event.should_receive(:publish).with(name, hash_including(defaults.merge(data)))
     block.call
   end
 end
@@ -10,14 +10,14 @@ end
 RSpec::Matchers.define :publish_events do |*args|
 
   match do |block|
-    defaults = {:actor => anything, :target => anything, at: nil}
+    defaults = {:actor => anything}
 
     args.each do |event|
       case event
         when Symbol
           Reactor::Event.should_receive(:publish).with(event, anything)
         when Hash
-          Reactor::Event.should_receive(:publish).with(event.keys.first, defaults.merge(event.values.first))
+          Reactor::Event.should_receive(:publish).with(event.keys.first, hash_including(defaults.merge(event.values.first)))
       end
     end
     block.call
