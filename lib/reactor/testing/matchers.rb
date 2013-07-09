42605b4d8523db310048572d827de1dd664d5f9e
williamsmith7271 <williamsmith7271@gmail.com>
2013-07-09 11:26:25 -0700

    improve flexibility of publish_events matcher

--- lib/reactor/testing/matchers.rb
+++ lib/reactor/testing/matchers.rb
@@ -7,17 +7,18 @@ RSpec::Matchers.define :publish_event do |name, data = {}|
   end
 end
 
-RSpec::Matchers.define :publish_events do |*args|
+RSpec::Matchers.define :publish_events do |*events|
 
   match do |block|
-    defaults = {:actor => anything}
+    defaults = {:actor => anything, at: anything, target: anything}
 
-    args.each do |event|
-      case event
-        when Symbol
-          Reactor::Event.should_receive(:publish).with(event, anything)
-        when Hash
-          Reactor::Event.should_receive(:publish).with(event.keys.first, hash_including(defaults.merge(event.values.first)))
+    Reactor::Event.should_receive(:publish).exactly(events.count).times.with do |event, data|
+      match = events.select { |e| (e.is_a?(Hash) ? e.keys.first : e) == event }.first
+      match.should be_present
+
+      expected = match.is_a?(Hash) ? match.values.first : {match => {}}
+      expected.each do |key, value|
+        value.should == expected[key]
       end
     end
     block.call
