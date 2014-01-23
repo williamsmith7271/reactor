604bc766c979ee825506e90391b8abdaec1d588b
williamsmith7271 <williamsmith7271@gmail.com>
2014-01-23 10:29:35 -0800

    fix publish_event and publish_events RSpec matchers

--- lib/reactor/testing/matchers.rb
+++ lib/reactor/testing/matchers.rb
@@ -1,17 +1,20 @@
 RSpec::Matchers.define :publish_event do |name, data = {}|
-
   match do |block|
     defaults = {:actor => anything}
     Reactor::Event.should_receive(:publish).with(name, hash_including(defaults.merge(data)))
-    block.call
+
+    begin
+      block.call
+      RSpec::Mocks::verify
+      true
+    rescue RSpec::Mocks::MockExpectationError => e
+      false
+    end
   end
 end
 
 RSpec::Matchers.define :publish_events do |*events|
-
   match do |block|
-    defaults = {:actor => anything, at: anything, target: anything}
-
     Reactor::Event.should_receive(:publish).exactly(events.count).times.with do |event, data|
       match = events.select { |e| (e.is_a?(Hash) ? e.keys.first : e) == event }.first
       match.should be_present
@@ -21,12 +24,18 @@ RSpec::Matchers.define :publish_events do |*events|
         value.should == expected[key]
       end
     end
-    block.call
+
+    begin
+      block.call
+      RSpec::Mocks::verify
+      true
+    rescue RSpec::Mocks::MockExpectationError => e
+      false
+    end
   end
 end
 
 RSpec::Matchers.define :subscribe_to do |name, data = {}, &expectations|
-
   match do
     expectations.call if expectations.present?
     Reactor::Event.publish(name, data)
